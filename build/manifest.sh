18bb1d2f3f9d9e3d356ee51060be1ec16596a465
Tomer Cohen <tomerc+github.com@gmail.com>
2014-01-04 02:51:41 +0200

    bump version string and track css file(s)

--- build/manifest.sh
+++ build/manifest.sh
@@ -1,6 +1,6 @@
 echo Creating manifest file
 cd ..
 echo CACHE MANIFEST   > manifest.appcache
-echo \# v1.0.3 `date` >> manifest.appcache
-ls *.html *.otf       >> manifest.appcache
+echo \# v1.0.4 `date` >> manifest.appcache
+ls *.html *.otf *.css >> manifest.appcache
 ls res/*.json         >> manifest.appcache
