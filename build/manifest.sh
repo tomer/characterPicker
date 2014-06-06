36c98452d5688eb8c86b5afb11808947fc4f8fbe
Tomer Cohen <tomerc+github.com@gmail.com>
2014-06-07 02:01:55 +0300

    add ttf support to appcache build script

--- build/manifest.sh
+++ build/manifest.sh
@@ -2,6 +2,6 @@ echo Creating manifest file
 cd ..
 echo CACHE MANIFEST   > manifest.appcache
 echo \# v1.0.4 `date` >> manifest.appcache
-ls *.html *.js  *.css >> manifest.appcache
-ls *.otf              >> manifest.appcache
+ls *.html *.js *.css  >> manifest.appcache
+ls *.otf *.ttf        >> manifest.appcache
 ls res/*.json         >> manifest.appcache
