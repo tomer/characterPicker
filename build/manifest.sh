2e454dc63c20c4782a401bf1280210f2076ae147
Tomer Cohen <tomerc+github.com@gmail.com>
2014-06-28 18:57:29 +0300

    incresing manifest.appcache version

--- build/manifest.sh
+++ build/manifest.sh
@@ -1,7 +1,7 @@
 echo Creating manifest file
 cd ..
 echo CACHE MANIFEST   > manifest.appcache
-echo \# v1.0.4 `date` >> manifest.appcache
+echo \# v1.0.5 `date` >> manifest.appcache
 ls *.html *.js *.css  >> manifest.appcache
 ls *.otf *.ttf        >> manifest.appcache
 ls res/*.json         >> manifest.appcache
