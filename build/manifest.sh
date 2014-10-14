f46a33337bb899380d4fc79c39fdfe34fa0f1489
Tomer Cohen <tomerc+github.com@gmail.com>
2014-10-15 01:11:24 +0300

    Add NETWORK section to the offline manifest

--- build/manifest.sh
+++ build/manifest.sh
@@ -1,7 +1,12 @@
 echo Creating manifest file
 cd ..
 echo CACHE MANIFEST   > manifest.appcache
-echo \# v1.0.5 `date` >> manifest.appcache
+echo \# v1.0.6 `LANG=en date` >> manifest.appcache
+echo                  >> manifest.appcache
+echo CACHE:           >> manifest.appcache
 ls *.html *.js *.css  >> manifest.appcache
 ls *.otf *.ttf        >> manifest.appcache
 ls res/*.json         >> manifest.appcache
+echo                  >> manifest.appcache
+echo NETWORK:         >> manifest.appcache
+echo \*               >> manifest.appcache
